c37b72c5e29cfb2c7bf607f39499b28098f82c0c
stephen <isaacsteverado9@gmail.com>
2025-02-22 01:58:41 +0100

    Done with https

---
+++ asyncAwait.js
@@ -0,0 +1,26 @@
+const urls = [
+    'https://jsonplaceholder/typicode.com/users',
+    'https://jsonplaceholder/typicode.com/posts',
+    'https://jsonplaceholder/typicode.com/albums'
+]
+
+Promise.all(urls.maps(url => {
+    fetch(url).then(resp => resp.json())
+})).then(array => {
+    console.log('users', array[0])
+    console.log('posts', array[1])
+    console.log('albums', array[2])
+}).catch('oops');
+
+const getData = async function() {
+    try {
+        const [users, posts, albums ] = await Promise.all(urls.map(url => 
+            fetch(url).then(resp => resp.json())
+        ))
+        console.log('users', users)
+        console.log('posts', posts)
+        console.log('albums', albums)  
+    } catch (err) {
+        console.log('ooops', err);
+    }
+}
